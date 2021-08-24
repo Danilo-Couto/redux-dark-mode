da756da8c6d09294cd03c0ac2719cf0b2e18cf56
EduardoSantosF <eduardo.debest81@gmail.com>
2021-08-24 18:07:59 -0300

    create routes and provider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { Route, Switch } from 'react-router-dom';
+
 import DarkModeButton from './components/DarkModeButton';
+import GithubList from './components/GithubList';
+import Login from './components/Login';
+
 import './App.css';
 
 function App() {
   return (
-    <>
-      <header className="header">
+    <div>
+      <header>
         <DarkModeButton />
       </header>
-    </>
+      <Switch>
+        <Route path="/" component={GithubList} />
+        <Route path="login" component={Login} />
+      </Switch>
+    </div>
   );
 }
 
