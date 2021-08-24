1a2c4253d4812c04b6a80609cb00a199faa0fa43
EduardoSantosF <eduardo.debest81@gmail.com>
2021-08-24 19:59:30 -0300

    req 9 done

--- src/components/GithubList.js
+++ src/components/GithubList.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { actionGetRepoList } from '../redux/actions'
+import { actionGetRepoList, actionLogout } from '../redux/actions'
 
 export class GithubList extends Component {
   componentDidMount() {
     const { dispatchRepoList, gitHubUser, history } = this.props;
-    console.log(gitHubUser, 'update');
     if (gitHubUser) dispatchRepoList(gitHubUser);
     else {
       alert('Você precisa estar logado para acessar está página!');
       history.push('login');
     }
   }
+
+  onLogout = () => {
+    const { dispatchLogout, history} = this.props;
+    dispatchLogout();
+    history.push('/login');
+  }
   render() {
     const { repoList } = this.props;
     return (
       <div>
+        <button onClick={this.onLogout}>Logout</button>
         {repoList.map((repo, index) => (
           <h3 key={index}>{repo}</h3>
         ))}
@@ -30,6 +36,7 @@ const mapStateToProps = ({repoList, user: { gitHubUser }}) => ({
 
 const mapDispatchToProps = (dispatch) => ({
   dispatchRepoList: (gitHubUser) => dispatch(actionGetRepoList(gitHubUser)),
+  dispatchLogout: () => dispatch(actionLogout()),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(GithubList)
