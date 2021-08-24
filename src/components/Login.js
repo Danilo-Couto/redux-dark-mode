68bb6899085adb6dcaac65f2950868de29e4c4ae
EduardoSantosF <eduardo.debest81@gmail.com>
2021-08-24 18:18:21 -0300

    Login page done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,56 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { actionLogin } from '../redux/actions'
 
 export class Login extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: '',
+      gitHubUser: '',
+    }
+  }
+
+  onSubmit = (e) => {
+    const { email, gitHubUser } = this.state;
+    const { dispatchLogin } = this.props;
+    e.preventDefault();
+    dispatchLogin(email, gitHubUser);
+  }
+
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+
   render() {
     return (
-      <div>
-        
-      </div>
+      <form onSubmit={this.onSubmit}>
+        <label htmlFor="email">
+          <input 
+            type="email" 
+            name="email" 
+            id="email" 
+            placeholder="Email" 
+            onChange={this.onChange} />
+        </label>
+        <label htmlFor="gitHubUser">
+          <input 
+            type="text" 
+            name="gitHubUser" 
+            id="gitHubUser" 
+            placeholder="Git User" 
+            onChange={this.onChange} />
+        </label>
+        <input type="submit" value="Login" />
+      </form>
     )
   }
 }
 
-const mapStateToProps = (state) => ({
-  
+const mapDispatchToProps = (dispatch) => ({
+  dispatchLogin: (email, gitHubUser) => dispatch(actionLogin(email, gitHubUser)),
 })
 
-const mapDispatchToProps = {
-  
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Login)
+export default connect(null, mapDispatchToProps)(Login)
